89830a7319b35aa0bc89991523a8b419b9e72008
0273898 <0273898@KELCE-LABS13-P.pittstate.edu>
2019-09-19 12:07:17 -0500

    end of class code

--- src/main/java/Ch5.java
+++ src/main/java/Ch5.java
@@ -1,15 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author 0273898
- */
+import java.util.Scanner;
+
 public class Ch5 {
     public static void main(String[] args) {
         
+       addTwoNumbersAndPrint(1,2);
+       
+       addTwoNumbersAndPrint(5,5);
+       
+       System.out.print("Enter two numbers to add together: ");
+       Scanner keyboard = new Scanner(System.in);
+       
+       double firstNumber = keyboard.nextDouble();
+       double secondNumber = keyboard.nextDouble();
+       
+       addTwoNumbersAndPrint(firstNumber, secondNumber);
+       
+        
+    }
+    
+    public static void addTwoNumbersAndPrint(double firstNumber, double secondNumber)
+    {
+        double sum = firstNumber + secondNumber;
+        
+        System.out.println("The sum is: " + sum);
+                
     }
 }
